2c07704ab986c811b7518b5898ccd94e07ceae75
sandun dimantha <sandundimantha58@gmail.com>
2025-09-19 20:13:00 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,22 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("you got 20 notes");
+    res.send("you got 10 notes");
 });
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"note created successfully !"});
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"note updated successfully !"});
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"note delete successfully !"});
+});
+
+
 
 app.listen(5001, () => {
     console.log("server started pm PORT:5001");
