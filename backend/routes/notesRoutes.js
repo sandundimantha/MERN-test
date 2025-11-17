c3851aa062943fc0d81b3add1e3d48500a8c4521
sandun dimantha <sandundimantha58@gmail.com>
2025-11-17 13:29:51 +0530

    setup the small error

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,15 +1,11 @@
 import express from "express";
-import {getAllNotes } from "../controllers/notesController.js";
+import { getAllNotes, createNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes); 
 
 router.post("/", createNote); 
-
-router.post("/", (req, res) => {
- res.status(201).json({message:"note created successfully !"});
-});
 
 router.put("/:id", (req, res) => {
 res.status(200).json({message:"note updated successfully !"});
